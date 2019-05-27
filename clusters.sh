32f840b2edf997ccce67ed27d054fb8cb544d1de
Lykopis Alcinoe <sa@Lykopiss-MacBook-Pro.local>
2019-05-27 11:28:04 +0200

    corrections

--- clusters.sh
+++ clusters.sh
@@ -3,9 +3,9 @@
 outputpath=results/clusters
 mkdir -p $outputpath
 
-python -u cluster.py 3 3 4
+python -u cluster.py 3 3 3
 
-git pull
-git add $outputpath/*
-git commit -m "Cluster pictures added by server."
-git push
+# git pull
+# git add $outputpath/*
+# git commit -m "Cluster pictures added by server."
+# git push
