42cbe434ba4b5752ebb2403000a416c843a23410
S.Hyatt <smhyatt@protonmail.com>
2019-06-03 16:18:08 +0200

    Additions.

---
+++ init.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='ethereum_framework_utils',
+        description='Helper functions for Ethereum Framework',
+        author='Sarah Hyatt',
+        author_email='zfj900@alumni.ku.dk',
+        version='1.0',
+        install_requires=['scikit-learn', 'h5py']
+     )
